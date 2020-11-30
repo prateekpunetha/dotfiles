08729537b5c175e0a9f4e815aeef15d94cbe5893
prateekpunetha <hi@prateekpunetha.dev>
2020-11-30 09:49:17 +0530

    dots: Cleanup Unused
    Signed-off-by: prateekpunetha <hi@prateekpunetha.dev>

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -27,42 +27,5 @@ fi
 # various aliases.
 alias free="free -h"
 alias fdisk="sudo fdisk -l"
-alias c="bat " # this one is way better
 alias zsh="exec zsh"
 alias n="nvim"
-alias py="python3"
-
-# packages aliases.
-alias y="yay -Syu --noconfirm"
-alias remove="yay -Rds"
-alias install="yay -S"
-alias pinfo="yay -Qi " # get info of an installed package.
-alias orphan="yay -Rns $(pacman -Qtdq)" # remove orphaned packages.
-alias cleanc="yay -Scc" # clean cached packages files.
-
-# git aliases.
-alias gs="git status"
-alias ga="git add"
-alias gaa="git add -A"
-alias gc="git commit --signoff"
-alias gcp="git cherry-pick"
-alias gl="git log | bat"
-alias gr="git remote"
-alias gra="git remote add"
-alias grm="git remote rm"
-alias grv="git remote -v"
-alias gd="git diff"
-alias gpl="git pull"
-alias gclean="git reflog expire --all --expire=now && git gc --prune=now --aggressive"
-
-# Directory hashes.
-hash -d disk="/media/Dark-Files"
-hash -d dots="/home/blacksuan19/.dotfiles"
-hash -d git="/media/Dark-Files/Gits"
-hash -d dav="/media/Dark-Files/Android-DEV/kernel/vince"
-hash -d dax="/media/Dark-Files/Android-DEV/kernel/phoenix"
-hash -d trees="/media/Dark-Files/Android-DEV/Trees"
-hash -d vince="/media/Dark-Files/Android-DEV/Trees/vince"
-hash -d ph="/media/Dark-Files/Android-DEV/Trees/phoenix"
-hash -d flutter="/media/Dark-Files/Flutter"
-hash -d kul="/media/Dark-Files/Kulliyya"
