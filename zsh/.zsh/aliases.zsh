781c20387b6547927adae495d64335a7bb78868f
prateekpunetha <hi@prateekpunetha.dev>
2023-04-06 08:22:16 +0530

    aliases: add alias for youtube-dl
    Signed-off-by: prateekpunetha <hi@prateekpunetha.dev>

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -12,6 +12,7 @@ alias copy="xclip -selection clipboard"
 alias rangercd=". ranger"
 alias alive="systemd-inhibit --what=handle-lid-switch sleep"
 alias wget="wget --hsts-file /tmp/wgethist.txt"
+alias yt-dlp="yt-dlp -o '%(title)s.%(ext)s' --external-downloader aria2c --external-downloader-args '-c -j 4 -x 8 -s 8 -k 1M'"
 
 # wireguard
 alias up="systemctl start wg-quick@wg0 --now"
